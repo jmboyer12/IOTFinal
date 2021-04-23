97fb0df6853f2de6d9550b3718ee3d9bc4efd8ce
jmboyer12 <boyerjoe6@gmail.com>
2021-04-23 19:22:27 +0100

    Added bluetooth Please don't add node_modules to git repository

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const { createBluetooth } = require( 'node-ble' );
 var io = require('socket.io-client')
 
-//Dillon was here
 // TODO: Replace this with your Arduino's Bluetooth address
 // as found by running the 'scan on' command in bluetoothctl
 const ARDUINO_BLUETOOTH_ADDR = '67:66:24:8B:8E:00';
@@ -13,38 +12,61 @@ const ESS_SERVICE_UUID = '0000181A-0000-1000-8000-00805F9B34FB';
 const TEMP_CHARACTERISTIC_UUID = '00002A6E-0000-1000-8000-00805F9B34FB';
 
 
-async function main(){
+//comms: each type of code will be superceded by a character identifier
+const stateManagement = 's';
 
-    //BLUETOOTH CONNECTION
-     // Attempt to connect to the device with specified BT address
-    //  const device = await adapter.waitDevice( ARDUINO_BLUETOOTH_ADDR.toUpperCase() );
-    //  console.log( 'found device. attempting connection...' );
-    //  await device.connect();
-    //  console.log( 'connected to device!' );
- 
-    //  // Get references to the desired UART service and its characteristics
-    //  const gattServer = await device.gatt();
-    //  const uartService = await gattServer.getPrimaryService( UART_SERVICE_UUID.toLowerCase() );
-    //  const txChar = await uartService.getCharacteristic( TX_CHARACTERISTIC_UUID.toLowerCase() );
-    //  const rxChar = await uartService.getCharacteristic( RX_CHARACTERISTIC_UUID.toLowerCase() );
-    //  const essService = await gattServer.getPrimaryService( ESS_SERVICE_UUID.toLowerCase() );
-    //  const tempChar = await essService.getCharacteristic( TEMP_CHARACTERISTIC_UUID.toLowerCase() );
-
-
-
+var comms = {};
+var socket = io.connect('https://iot-project-server.glitch.me/');
+console.log("SocketIO ready");
     //SOCKETIO STUFF
-    var socket = io.connect('https://iot-project-server.glitch.me/');
+socket.on('toggle', data => {
+    if(comms.tx){
+        comms.tx.writeValue(Buffer.from(stateManagement + '0')).then(()=>{console.log('toggle')}).catch(err =>{
+            console.log('Error writing toggle: ' + err)
+        });
+    }    
+})
 
-    socket.on('toggle', data => {
-        console.log('toggle');
-    })
+socket.on('increment', data => {
+    if(comms.tx){
+        comms.tx.writeValue(Buffer.from(stateManagement + '1')).then(()=>{console.log('increment')}).catch(err =>{
+            console.log('Error writing increment: ' + err)
+        });
+    }
+    
+})
 
-    socket.on('increment', data => {
-        console.log('increment');
-    })
+socket.on('decrement', data => {
+    if(comms.tx){
+        comms.tx.writeValue(Buffer.from(stateManagement + '2')).then(()=>{console.log('decrement')}).catch(err =>{
+            console.log('Error writing decrement: ' + err)
+        });
+    }
+})
 
-    socket.on('decrement', data => {
-        console.log('decrement');
+async function main(){
+    const { bluetooth, destroy } = createBluetooth();
+    bluetooth.defaultAdapter().then(async adapter =>{
+        await adapter.startDiscovery()
+        return adapter;
+    }).then(adapter => {
+        console.log("discovering...")
+        return adapter.waitDevice( ARDUINO_BLUETOOTH_ADDR.toLocaleUpperCase() );
+    }).then(async device => {
+        console.log('Found device');
+        await device.connect();
+        return device;
+    }).then(device =>{
+        console.log('Connected');
+        return device.gatt();
+    }).then(gattServer => {
+        return gattServer.getPrimaryService( UART_SERVICE_UUID.toLowerCase() );
+    }).then(async uartService => {
+        comms.tx = await uartService.getCharacteristic( TX_CHARACTERISTIC_UUID.toLowerCase() );
+        comms.rx = await uartService.getCharacteristic( RX_CHARACTERISTIC_UUID.toLowerCase() );
+        console.log('Got UART');
+    }).catch(err => {
+        console.log(err);
     })
 }
-main();
+main()
