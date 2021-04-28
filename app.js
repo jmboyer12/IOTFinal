9207a9e4f4adb4db7a3d3d86df372a851125799c
dsafrica <dillon-africa@uiowa.edu>
2021-04-28 16:00:23 -0500

    Changes made?

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var io = require('socket.io-client')
 
 // TODO: Replace this with your Arduino's Bluetooth address
 // as found by running the 'scan on' command in bluetoothctl
-const ARDUINO_BLUETOOTH_ADDR = '67:66:24:8B:8E:00';
+const ARDUINO_BLUETOOTH_ADDR = '3B:E9:94:0F:79:00';
 
 const UART_SERVICE_UUID      = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'; 
 const TX_CHARACTERISTIC_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E';
@@ -43,6 +43,7 @@ socket.on('decrement', data => {
         });
     }
 })
+
 
 async function main(){
     const { bluetooth, destroy } = createBluetooth();
