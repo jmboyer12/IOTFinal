0392879b80ee2b0eb87e6912e284ef29ac4589fc
jmboyer12 <boyerjoe6@gmail.com>
2021-04-29 22:32:20 +0100

    Influx is up and running - currently waiting on Arduino

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const { createBluetooth } = require( 'node-ble' );
 var io = require('socket.io-client')
 
+// import/reference to the library
+const Influx = require("influx");
+const os = require('os')
+const http = require('http');
 
 // TODO: Replace this with your Arduino's Bluetooth address
 // as found by running the 'scan on' command in bluetoothctl
@@ -11,11 +15,11 @@ const TX_CHARACTERISTIC_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E';
 const RX_CHARACTERISTIC_UUID = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E';
 const ESS_SERVICE_UUID = '0000181A-0000-1000-8000-00805F9B34FB';
 const TEMP_CHARACTERISTIC_UUID = '00002A6E-0000-1000-8000-00805F9B34FB';
-
+const dimming_measurement = 'dimming_intensity';
 
 //comms: each type of code will be superceded by a character identifier
 const stateManagement = 's';
-
+const intensity_poll = 'p';
 var comms = {};
 var socket = io.connect('https://iot-project-server.glitch.me/');
 console.log("SocketIO ready");
@@ -80,109 +84,100 @@ main()
 // FOR RICK
 // this function is called everytime Dillon sends a message via UART
 // TO DO: Parsing of some sort - dependent on the data being sent
+
 const messageFromArduino = (buffer) => {
     console.log(buffer.toString());
+    if(buffer[0] == 's'){
+        // mess with later
+    }
+    else if (buffer[0] == 'p'){
+        const arduinoMessage = buffer.toString();
+        const messageParse = parseInt(arduinoMessage.slice(1,arduinoMessage.length-1)) 
+        console.log(messageParse);
+        writeToInflux(messageParse);
+    }
 }
-/*
+
+
+const writeToInflux = (dimming_intensity) => {
+    // verify the influx server exists
+    if(!influx){
+        return;
+    }
+    influx.writePoints([
+        {
+          measurement: 'user1',
+          tags: { host: os.hostname() },
+          fields: { dimming_measurement: dimming_intensity },
+        }
+      ]).then(()=>{
+        return true;
+      }).catch(err => {
+        console.error(`Error saving data to InfluxDB! ${err.stack}`)
+      })
+}
+
+const queryInflux = () => {
+    if(!influx){
+        return;
+    }
+    influx.query(
+        `select * from user1`
+    )
+    .then(result => {
+        result.forEach(row => console.log(row.dimming_measurement))
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+setInterval(() => {
+    // transmit to Arduino requesting light intensity :)
+    if(comms.tx){
+        comms.tx.writeValue(Buffer.from(intensity_poll)).then(()=>{console.log('Intensity Poll: ')}).catch(err =>{
+            console.log('Error transmitting ' + err)
+        });
+    }
+}, 10000);
+
 // FOR RICK
 // TO DO: Need InfluxDB and have the ability to send stuff to the InfluxDB - See Lab 4
 // Periodically poll Arduino for light intensity (DIMMER PERCENTAGE) and store returned value in InfluxDB
-/**
- * In this example we'll create a server which has an index page that prints
- * out "hello world", and a page `http://localhost:3000/times` which prints
- * out the last ten response times that InfluxDB gave us.
- *
- * Get started by importing everything we need!
- */
-/**
- const Influx = require("../../");
- const express = require("express");
- const http = require("http");
- const os = require("os");
- 
- const app = express();
- 
- /**
-  * Create a new Influx client. We tell it to use the
-  * `express_response_db` database by default, and give
-  * it some information about the schema we're writing.
+
+
  const influx = new Influx.InfluxDB({
    host: "localhost",
-   database: "express_response_db",
+   database: "light_readings_db",
    schema: [
      {
-       measurement: "response_times",
+       measurement: "user1",
        fields: {
-         path: Influx.FieldType.STRING,
-         duration: Influx.FieldType.INTEGER,
+         dimming_measurement: Influx.FieldType.FLOAT,
        },
        tags: ["host"],
      },
    ],
  });
- 
- /**
-  * Next we define our middleware and hook into the response stream. When it
-  * ends we'll write how long the response took to Influx!
-  
- app.use((req, res, next) => {
-   const start = Date.now();
- 
-   res.on("finish", () => {
-     const duration = Date.now() - start;
-     console.log(`Request to ${req.path} took ${duration}ms`);
- 
-     influx
-       .writePoints([
-         {
-           measurement: "response_times",
-           tags: { host: os.hostname() },
-           fields: { duration, path: req.path },
-         },
-       ])
-       .catch((err) => {
-         console.error(`Error saving data to InfluxDB! ${err.stack}`);
-       });
-   });
-   return next();
- });
- 
- app.get("/", function (req, res) {
-   setTimeout(() => res.end("Hello world!"), Math.random() * 500);
- });
- 
- app.get("/times", function (req, res) {
-   influx
-     .query(
-       `
-     select * from response_times
-     where host = ${Influx.escape.stringLit(os.hostname())}
-     order by time desc
-     limit 10
-   `
-     )
-     .then((result) => {
-       res.json(result);
-     })
-     .catch((err) => {
-       res.status(500).send(err.stack);
-     });
- });
- 
- /**
-  * Now, we'll make sure the database exists and boot the app.
-  
- influx
-   .getDatabaseNames()
+   
+ influx.getDatabaseNames()
    .then((names) => {
      if (!names.includes("express_response_db")) {
        return influx.createDatabase("express_response_db");
      }
    })
+   .then(infl =>{
+       return writeToInflux(5)
+   }).then(result =>{
+    queryInflux();
+   })
    .catch((err) => {
-     console.error(`Error creating Influx database!`);
+     console.error(`Error creating Influx database!` + err);
    });
-   */
+   
+
+
+   
 
 /** 
     // More API functions here:
